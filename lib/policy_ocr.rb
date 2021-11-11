0ba3c3f191b43256e22affcca083962ae51a9895
StevenZambito <zambito.steven@icloud.com>
2021-11-11 09:51:44 -0500

    Able to convert first 3 lines to real numbers (integers)

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -3,18 +3,46 @@ module PolicyOcr
   file = File.open("../spec/fixtures/sample.txt")
   file_data = file.read
 
-  lines = File.foreach("../spec/fixtures/sample.txt").first(4)
+  lines = File.foreach("../spec/fixtures/sample.txt").first(3)
 
+  numberHash = {
+    " _ | ||_|" => 0,
+  
+    "     |  |" => 1,
+  
+    " _  _||_ " => 2,
+  
+    " _  _| _|" => 3,
+  
+    "   |_|  |" => 4,
+  
+    " _ |_  _|" => 5,
+  
+    " _ |_ |_|" => 6,
+  
+    " _   |  |" => 7,
+  
+    " _ |_||_|" => 8,
+  
+    " _ |_| _|" => 9
+  }
 
+  counter = 0
+  counteragain = 2
 
-  zero = " _ \n| |\n|_|"
-  two = " _ \n _|\n|_ "
-  three = " _ \n _|\n _|"
-  four = "|_|\n  |"
-  five = " _ \n|_ \n _|"
-  six = " _ \n|_ \n|_|"
-  seven = " _ \n  |\n  |"
-  eight = " _ \n|_|\n|_|"
-  nine = " _ \n|_|\n _|"
+  until counteragain > 27
+    full_digit = ""
+    lines.each{|l| 
+
+      part_of_digit = l[counter..counteragain]
+
+      full_digit = full_digit + part_of_digit
+    }
+    counter+=3
+    counteragain+=3
+
+    puts numberHash[full_digit]
+    
+  end
 
 end
