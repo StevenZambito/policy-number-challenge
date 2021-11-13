b8fa6e0d0d30b891bb7bb47c564e858a882e9dc3
StevenZambito <zambito.steven@icloud.com>
2021-11-13 18:49:15 -0500

    Created sample file with varied line lengths and wrote test to check length of first 3 elements in outputted array of policy numbers

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -10,15 +10,34 @@ describe PolicyOcr do
   end
 
   describe "create_policy_number" do
-    it "makes a policy number from sample file" do
-      array = 
-      [
-        000000000, 111111111, 222222222, 333333333, 
-        444444444, 555555555, 666666666, 777777777, 
-        888888888, 999999999, 123456789      
-      ]
-        expect(PolicyOcr.create_policy_number('../spec/fixtures/sample.txt')).to eq(array)
+    context "'sample.txt' as input" do
+      it "creates policy numbers from sample file" do
+        expected_policy_numbers = 
+        [
+          000000000, 111111111, 222222222, 333333333, 
+          444444444, 555555555, 666666666, 777777777, 
+          888888888, 999999999, 123456789      
+        ]
+          expect(PolicyOcr.create_policy_number('spec/fixtures/sample.txt')).to eq(expected_policy_numbers)
+      end
+
+      it "outputs an array of integers" do
+        result = PolicyOcr.create_policy_number('spec/fixtures/sample.txt')
+        expect(result.all?{|num| num.is_a? Integer}).to be true
+      end
+
+      it "expected number of elements total (1 policy number per 4 lines)" do
+        expect(PolicyOcr.create_policy_number('spec/fixtures/sample.txt').length).to eq(11)
+      end
     end
 
-end
+    context "'varied_length_sample.txt'" do
+      it "makes policy numbers for longer and shorter lines (more and less characters)" do
+        result = PolicyOcr.create_policy_number('spec/fixtures/varied_length_sample.txt')
+        expect(result[0].to_s.length).to eq(6)
+        expect(result[1].to_s.length).to eq(11)
+        expect(result[2].to_s.length).to eq(12)
+      end
+    end
+  end
 end
